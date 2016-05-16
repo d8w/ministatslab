12fe81130cd069b09e4f0e525a23c6cdd13eb471
Donghan Wang <donghanw@cs.cmu.edu>
2016-05-16 17:01:26 -0400

    updated ui

--- ui/demo/ministats-main-app.js
+++ ui/demo/ministats-main-app.js
@@ -2,7 +2,7 @@
 
 ministatsDemoApp
 
-.controller('ministatsDemoAppCtrl', [ '$scope','$http', function($scope, $http) { 
+.controller('ministatsDemoAppCtrl', [ '$scope','$http', '$timeout', function($scope, $http, $timeout) { 
 
     $scope.form = {
         x: [0.29998503,  0.25058272,  0.62110361,  0.09335537,  0.23726673],
@@ -16,15 +16,25 @@ ministatsDemoApp
         $scope.submitted = true;
 
         var request = {
-            method  : 'GET',
+            method  : 'POST',
             url     : 'http://127.0.0.1:5000/api/linregress',
             data    : {x:$scope.form.x, y:$scope.form.y}
         } 
 
         $http(request)
             .then(function(success){
-                console.log(success);
-                $scope.submitted = false;
+                request = {
+                    method  : 'GET',
+                    url     : success.config.url + '/' + success.data._id
+                }
+                $http(request)
+                    .then(function(success){
+                        console.log(success);
+                        $scope.submitted = false;
+                    },function(error){
+                        console.log(error);
+                        $scope.submitted = false;
+                    });
             }, function(error){
                 console.log(error);
                 $scope.submitted = false;
